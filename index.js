e0b33265c9b875a87983acc51b35a199d9d54df7
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-24 08:41:01 +0100

    Connect to db

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const dbConnect = require("./config/dbConnect");
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 400;
+
+dbConnect()
+
+app.use('/', (req, res) => {
+    res.send("Hello world again!")
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`)
