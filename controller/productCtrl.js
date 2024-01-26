38ac6dea8de464dcd27e0c08a9046a66011343e6
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-26 09:39:33 +0100

    fix bugs: asyncHandler fxn in productCtrl & products route in productRoute

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const product = require("../models/productModel");
 
 
-const createProduct = syncHandler(
+const createProduct = asyncHandler(
     async (req, res) => {
         res.send('Hey these are my products')
     }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { createProduct } = require('../controller/productCtrl');
 
 
-router.post('/procducts', createProduct)
+router.post('/products', createProduct)
 
 
 module.exports = router
