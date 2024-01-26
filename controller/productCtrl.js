2e01e8e8d37df1f59d941dc1e121f9ba3a273a6a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-26 09:23:42 +0100

    create product  controller & route

---
+++ controller/productCtrl.js
@@ -0,0 +1,15 @@
+const asyncHandler = require("express-async-handler");
+const product = require("../models/productModel");
+
+
+const createProduct = syncHandler(
+    async (req, res) => {
+
+    }
+)
+
+
+
+module.exports = {
+    createProduct,
+};
