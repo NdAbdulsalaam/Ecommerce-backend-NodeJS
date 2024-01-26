5bbf92c7ab9819b9e2c77632a364054080caceb5
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-26 08:44:31 +0100

    Rearrage imports

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-const dbConnect = require("./config/dbConnect");
-const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 400;
-const authRouter = require('./routes/authRoute');
 const bodyParser = require('body-parser');
-const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 400;
+
+const dbConnect = require("./config/dbConnect");
+const authRouter = require('./routes/authRoute');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
+
 
 dbConnect()
 app.use(bodyParser.json()); 
