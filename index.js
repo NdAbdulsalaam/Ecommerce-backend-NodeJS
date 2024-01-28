6a9b116f6012698fdb051c77db11ff523a1d2267
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-28 16:32:06 +0100

    Update: include brandRouter for brands

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const dbConnect = require("./config/dbConnect");
 const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
 const blogRouter = require('./routes/blogRoute');
+const brandRouter = require('./routes/brandRoute');
 const prodCatRouter = require('./routes/prodCatRoute')
 const blogCatRouter = require('./routes/blogCatRoute')
 
@@ -28,8 +29,9 @@ app.use(cookieParser())
 app.use('/user', authRouter);
 app.use('/product', productRouter);
 app.use('/post', blogRouter); //NOTE: POST is used inplace of BLOG in routes & functions
+app.use('/brand', brandRouter);
 app.use('/product/category', prodCatRouter);
-app.use('/blog/category', blogCatRouter);
+app.use('/post/category', blogCatRouter);
 
 app.use(notFound)
 app.use(errorHandler) 
