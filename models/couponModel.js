8599a73cef2833152d1e7b99bcc5f6f06381739a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-29 17:42:15 +0100

    create: couponModel

---
+++ models/couponModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var couponSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        uupercase:true,
+    },
+    expiry:{
+        type:Date,
+        required:true,
+    },
+    discount:{
+        type:Number,
+        required:true,
+    }
+},
+ { timestamps:true}
+);
+
+//Export the model
+module.exports = mongoose.model('Coupon', couponSchema);
