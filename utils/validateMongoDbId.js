8daf3b3b24fc26b076300636b4cb185ec8e2d2a0
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-29 20:50:51 +0100

    Update changes

--- utils/validateMongoDbId.js
+++ utils/validateMongoDbId.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const validateMongoDbId = (id) => {
     const isValid = mongoose.Types.ObjectId.isValid(id);
     if(!isValid) {
-        throw new Error("The database id is not valid")
+        throw new Error("This id is not valid")
     }
 }
 
