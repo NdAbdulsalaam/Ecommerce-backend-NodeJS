4e91ae40f375f7c8731b12ba44cf7e27d1d5d716
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-01-30 00:24:27 +0100

    create: upload, generate random name & resize images

---
+++ middlewares/imageMiddleware.js
@@ -0,0 +1,62 @@
+const multer = require('multer');
+const sharp = require('sharp');
+const path = require('path');
+
+const multerStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, '../public/images'));
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + "." + Math.round(Math.random() * 1e9);
+        cb(null, file.fieldname + "-" + uniqueSuffix + ".jpeg");
+    }
+});
+
+const multerFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(
+            { message: "unsupported file format" },
+            false
+        );
+    }
+};
+
+const uploadImage = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter,
+    limits: { fileSize: 2000000 }
+});
+
+const resizeProdImage = async (req, res, next) => {
+    if (!req.files) return next();
+    await Promise.all(
+        req.files.map(async (file) => {
+            await sharp(file.path).resize(300, 300)
+                .toFormat('jpeg')
+                .jpeg({ quality: 90 })
+                .toFile(`public/images/product/${file.filename}`);
+        })
+    );
+    next();
+};
+
+const resizeBlogImage = async (req, res, next) => {
+    if (!req.files) return next();
+    await Promise.all(
+        req.files.map(async (file) => {
+            await sharp(file.path).resize(300, 300)
+                .toFormat('jpeg')
+                .jpeg({ quality: 90 })
+                .toFile(`public/images/blog/${file.filename}`);
+        })
+    );
+    next();
+};
+
+module.exports = {
+    uploadImage,
+    resizeProdImage,
+    resizeBlogImage
+};
