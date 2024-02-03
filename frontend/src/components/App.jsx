1228666e6dcf0b960e4478f68c52ae566435cd6f
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-03 06:52:41 +0100

    update: setup to allow for page routing

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from '@/components/Layout';
+import Home from '@/pages/Home';
+
 
 const App = () => {
   return (
-    <h1>Hello react</h1>
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Layout /> }>
+                  <Route index element={ <Home /> } />
+                /</Route>
+            </Routes>
+        </BrowserRouter>
+    </>
   )
 }
 
