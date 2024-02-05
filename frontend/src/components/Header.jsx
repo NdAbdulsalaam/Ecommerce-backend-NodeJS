c9c46a4a8a0b1d8c667835565ba768a3eba1971f
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-05 22:03:02 +0100

    add: compare, wishlist, account & cart icons

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -42,7 +42,37 @@ function Header() {
                 </span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex justify-content-between">
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="/images/compare.svg" alt='Compare products' />
+                    <p className='mb-0'>Compare <br /> Products</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="/images/wishlist.svg" alt='Check wishlist' />
+                    <p className='mb-0'>Check <br /> wishlist</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src="/images/user.svg" alt='User' />
+                    <p className='mb-0'>Log in <br /> My Account</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white pt-2'>
+                    <img src="/images/cart.svg" alt='Cart' />
+                    <div className='d-flex flex-column gap-10 pt-1'>
+                      <span className='badge bg-white text-dark'></span>
+                      <p className='mb-0'>₦2,500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
