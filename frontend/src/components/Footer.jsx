4ae36c3fccc5621f4d6cd8e80994a439a5d97560
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-07 23:01:04 +0100

    Update changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,125 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <footer className="footer">
+		{/* Footer Top */}
+    <div className='section'>
+    <div className=' footer-newsletter'>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-5 col-md-6 col-12">
+              <div className="footer-top-data d-flex gap-30 align-items-center justify-content-center">
+                <img src='images/newsletter.png' />
+                <h4 className='text-white mb-0'>Sign Up For Our Newsletter</h4>
+              </div>
+            </div>
+            <div className='col-lg-6 col-md-6 col-12'>
+            <div className="input-group align-items-center">
+                <input type="text" className="form-control py-1" 
+                  placeholder="Your Email Address" 
+                  aria-label="Your Email Address"
+                  aria-describedby="basic-addon2" 
+                />
+                <span className="input-group-text p-1" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+		<div className="footer-top">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-5 col-md-6 col-12">
+						{/* Single Widget */}
+						<div className="single-footer about">
+							<div className="logo">
+								<a href="index.html"><img src="images/logo2.png" alt="#" /></a>
+							</div>
+							<p className="text">Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue,  magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</p>
+							<p className="call">Got Question? Call us 24/7<span><a href="tel:+23468874902">+2348168874902</a></span></p>
+						</div>
+						{/* End Single Widget */}
+					</div>
+					<div className="col-lg-2 col-md-6 col-12">
+						{/* Single Widget */}
+						<div className="single-footer links">
+							<h4>Information</h4>
+							<ul>
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Faq</a></li>
+								<li><a href="#">Terms & Conditions</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Help</a></li>
+							</ul>
+						</div>
+						{/* End Single Widget */}
+					</div>
+					<div className="col-lg-2 col-md-6 col-12">
+						{/* Single Widget */}
+						<div className="single-footer links">
+							<h4>Customer Service</h4>
+							<ul>
+								<li><a href="#">Payment Methods</a></li>
+								<li><a href="#">Money-back</a></li>
+								<li><a href="#">Returns</a></li>
+								<li><a href="#">Shipping</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+						</div>
+						{/* End Single Widget */}
+					</div>
+					<div className="col-lg-3 col-md-6 col-12">
+						{/* Single Widget */}
+						<div className="single-footer social">
+							<h4>Get In Tuch</h4>
+							{/* Single Widget */}
+							<div className="contact">
+								<ul>
+									<li>3 Tajudeen Ariyo Street </li>
+                  <li>Irawo Bus-stop, Ikorodu</li>
+									<li>Lagos, Nigeria</li>
+									<li>olaitansalaam2012</li>
+									<li>+2348168874902</li>
+								</ul>
+							</div>
+							{/* End Single Widget */}
+							<ul>
+								<li><a href="#"><i className="ti-facebook"></i></a></li>
+								<li><a href="#"><i className="ti-twitter"></i></a></li>
+								<li><a href="#"><i className="ti-flickr"></i></a></li>
+								<li><a href="#"><i className="ti-instagram"></i></a></li>
+							</ul>
+						</div>
+						{/* End Single Widget */}
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+		{/* End Footer Top */}
+		<div className="copyright">
+			<div className="container">
+				<div className="inner">
+					<div className="row">
+						<div className="col-lg-6 col-12">
+							<div className="left">
+								<p>Copyright © {new Date().getFullYear()} <a href="http://www.wpthemesgrid.com" target="_blank">Nurudeen Abdulsalaam</a>  -  All Rights Reserved.</p>
+							</div>
+						</div>
+						<div className="col-lg-6 col-12">
+							<div className="right">
+								<img src="images/payments.png" alt="#" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+    </div>
   )
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,146 +3,168 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      {/* Main Navigation */}
-        <header>
-        {/* Jumbotron */}
-        <div className="p-3 text-center bg-white border-bottom">
-            <div className="container">
-            <div className="row gy-3">
-                {/* Left elements */}
-                <div className="col-lg-2 col-sm-4 col-4">
-                <a href="/" target="_blank" className="float-start">
-                    <h2>Nuyola</h2>
-                    {/* <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" height="35" /> */}
-                </a>
-                </div>
-                {/* Left elements */}
-
-                {/* Center elements */}
-                <div className="order-lg-last col-lg-5 col-sm-8 col-8">
-                <div className="d-flex float-end">
-                    <a href="https://github.com/mdbootstrap/bootstrap-material-design" className="me-1 border rounded py-1 px-3 nav-link d-flex align-items-center" target="_blank">
-                        <i className="fas fa-user-alt m-1 me-md-2"></i>
-                        <p className="d-none d-md-block mb-0">Sign in</p>
-                        </a>
-                    <a href="https://github.com/mdbootstrap/bootstrap-material-design" className="me-1 border rounded py-1 px-3 nav-link d-flex align-items-center" target="_blank">
-                        <i className="fas fa-heart m-1 me-md-2"></i>
-                        <p className="d-none d-md-block mb-0">Wishlist</p>
-                        </a>
-                    <a href="https://github.com/mdbootstrap/bootstrap-material-design" className="border rounded py-1 px-3 nav-link d-flex align-items-center" target="_blank">
-                        <i className="fas fa-shopping-cart m-1 me-md-2"></i>
-                        <p className="d-none d-md-block mb-0">My cart</p>
-                        </a>
-                </div>
-                </div>
-                {/* Center elements */}
-
-                {/* Right elements */}
-                <div className="col-lg-5 col-md-12 col-12">
-                <div className="input-group float-center">
-                    <div className="form-outline">
-                    <input type="search" id="form1" className="form-control" />
-                    <label className="form-label" for="form1">Search</label>
-                    </div>
-                    <button type="button" className="btn btn-primary shadow-0">
-                    <i className="fas fa-search"></i>
-                    </button>
-                </div>
-                </div>
-                {/* Right elements */}
-            </div>
-            </div>
-        </div>
-        {/* Jumbotron */}
-
-        {/* Navbar */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-white">
-            {/* Container wrapper */}
-            <div className="container justify-content-center justify-content-md-between">
-            {/* Toggle button */}
-            <button
-                    className="navbar-toggler border py-2 text-dark"
-                    type="button"
-                    data-mdb-toggle="collapse"
-                    data-mdb-target="#navbarLeftAlignExample"
-                    aria-controls="navbarLeftAlignExample"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                    >
-                <i className="fas fa-bars"></i>
-            </button>
-
-            {/* Collapsible wrapper */}
-            <div className="collapse navbar-collapse" id="navbarLeftAlignExample">
-                {/* Left links */}
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                    <a className="nav-link text-dark" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">Categories</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">Hot offers</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">Gift boxes</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">Projects</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">Menu item</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link text-dark" href="#">Menu name</a>
-                </li>
-                {/* Navbar dropdown */}
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-                    Others
-                    </a>
-                    {/* Dropdown menu */}
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li>
-                        <a className="dropdown-item" href="#">Action</a>
-                    </li>
-                    <li>
-                        <a className="dropdown-item" href="#">Another action</a>
-                    </li>
-                    <li><hr className="dropdown-divider" /></li>
-                    <li>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                    </li>
-                    </ul>
-                </li>
-                </ul>
-                {/* Left links */}
-            </div>
-            </div>
-            {/* Container wrapper */}
-        </nav>
-        {/* Navbar */}
-        {/* Jumbotron */}
-        <div className="bg-primary text-white py-5">
-            <div className="container py-5">
-            <h1>
-                Best products & <br />
-                brands in our store
-            </h1>
-            <p>
-                Trendy Products, Factory Prices, Excellent Service
-            </p>
-            <button type="button" className="btn btn-outline-light">
-                Learn more
-            </button>
-            <button type="button" className="btn btn-light shadow-0 text-primary pt-2 border border-white">
-                <span className="pt-1">Purchase now</span>
-            </button>
-            </div>
-        </div>
-        {/* Jumbotron */}
-        </header>
+{/* Header */}
+	<header className="header shop">
+		{/* Topbar */}
+		<div className="topbar">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-4 col-md-12 col-12">
+						{/* Top Left */}
+						<div className="top-left">
+							<ul className="list-main">
+								<li><i className="ti-headphone-alt"></i> +2348168874902</li>
+								<li><i className="ti-email"></i> olaitansalaam2012@gmail.com</li>
+							</ul>
+						</div>
+						{/*/ End Top Left */}
+					</div>
+					<div className="col-lg-8 col-md-12 col-12">
+						{/* Top Right */}
+						<div className="right-content">
+							<ul className="list-main">
+								<li><i className="ti-location-pin"></i> <a href="#">Store location</a></li>
+								<li><i className="ti-alarm-clock"></i> <a href="#">Daily deal</a></li>
+								<li><i className="ti-user"></i> <a href="#">My account</a></li>
+								<li><i className="ti-power-off"></i><a href="login.html#">Login</a></li>
+							</ul>
+						</div>
+						{/* End Top Right */}
+					</div>
+				</div>
+			</div>
+		</div>
+		{/* End Topbar */}
+		<div className="middle-inner">
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-2 col-md-2 col-12">
+						{/* Logo */}
+						<div className="logo">
+							<a href="index.html"><img src="images/logo.png" alt="logo" /></a>
+						</div>
+						{/*/ End Logo */}
+						{/* Search Form */}
+						<div className="search-top">
+							<div className="top-search"><a href="#0"><i className="ti-search"></i></a></div>
+							{/* Search Form */}
+							<div className="search-top">
+								<form className="search-form">
+									<input type="text" placeholder="Search here..." name="search" />
+									<button value="search" type="submit"><i className="ti-search"></i></button>
+								</form>
+							</div>
+							{/*/ End Search Form */}
+						</div>
+						{/*/ End Search Form */}
+						<div className="mobile-nav"></div>
+					</div>
+					<div className="col-lg-8 col-md-7 col-12">
+						<div className="search-bar-top">
+							<div className="search-bar">
+								<select>
+									<option selected="selected">All Category</option>
+									<option>watch</option>
+									<option>mobile</option>
+									<option>kid’s item</option>
+								</select>
+								<form>
+									<input name="search" placeholder="Search Products Here..." type="search" />
+									<button className="btnn"><i className="ti-search"></i></button>
+								</form>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-2 col-md-3 col-12">
+						<div className="right-bar">
+							{/* Search Form */}
+							<div className="sinlge-bar">
+								<a href="#" className="single-icon"><i className="fa fa-heart-o" aria-hidden="true"></i></a>
+							</div>
+							<div className="sinlge-bar">
+								<a href="#" className="single-icon"><i className="fa fa-user-circle-o" aria-hidden="true"></i></a>
+							</div>
+							<div className="sinlge-bar shopping">
+								<a href="#" className="single-icon"><i className="ti-bag"></i> <span className="total-count">2</span></a>
+								{/* Shopping Item */}
+								<div className="shopping-item">
+									<div className="dropdown-cart-header">
+										<span>2 Items</span>
+										<a href="#">View Cart</a>
+									</div>
+									<ul className="shopping-list">
+										<li>
+											<a href="#" className="remove" title="Remove this item"><i className="fa fa-remove"></i></a>
+											<a className="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#" /></a>
+											<h4><a href="#">Woman Ring</a></h4>
+											<p className="quantity">1x - <span className="amount">$99.00</span></p>
+										</li>
+										<li>
+											<a href="#" className="remove" title="Remove this item"><i className="fa fa-remove"></i></a>
+											<a className="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#" /></a>
+											<h4><a href="#">Woman Necklace</a></h4>
+											<p className="quantity">1x - <span className="amount">$35.00</span></p>
+										</li>
+									</ul>
+									<div className="bottom">
+										<div className="total">
+											<span>Total</span>
+											<span className="total-amount">$134.00</span>
+										</div>
+										<a href="checkout.html" className="btn animate">Checkout</a>
+									</div>
+								</div>
+								{/*/ End Shopping Item */}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		{/* Header Inner */}
+		<div className="header-inner">
+			<div className="container">
+				<div className="cat-nav-head">
+					<div className="row justify-content-center"> 
+                    {/* Categories drop down was cut out from here */}
+						<div className="col-lg-9 col-12">
+							<div className="menu-area">
+								{/* Main Menu */}
+								<nav className="navbar navbar-expand-lg">
+									<div className="navbar-collapse">	
+										<div className="nav-inner">	
+											<ul className="nav main-menu menu navbar-nav">
+													<li className="active" ><a href="#">Home</a></li>
+													<li><a href="#">Product</a></li>												
+													<li><a href="#">Service</a></li>
+													<li><a href="#">Shop<i className="ti-angle-down"></i><span className="new">New</span></a>
+														<ul className="dropdown">
+															<li><a href="shop-grid.html">Shop Grid</a></li>
+															<li><a href="cart.html">Cart</a></li>
+															<li><a href="checkout.html">Checkout</a></li>
+														</ul>
+													</li>
+													<li><a href="#">Pages</a></li>									
+													<li><a href="#">Blog<i className="ti-angle-down"></i></a>
+														<ul className="dropdown">
+															<li><a href="blog-single-sidebar.html">Blog Single Sidebar</a></li>
+														</ul>
+													</li>
+													<li><a href="contact.html">Contact Us</a></li>
+												</ul>
+										</div>
+									</div>
+								</nav>
+								{/*/ End Main Menu */}	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		{/*/ End Header Inner */}
+	</header>
+	{/*/ End Header */}
     </div>
   )
 }
