bc7939c1b8253f3e8d3793f761abd9af9010978a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-07 17:16:02 +0100

    re-strategize: get components from MDR

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // import '@/styles/main.css' 
-import '@/styles/class.css'
+import '@/styles/main.css'
 
 import Layout from '@/components/Layout';
 import Home from '@/pages/Home';
