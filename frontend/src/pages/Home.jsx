e1fb352e4c410a7cb5bbde14489c52bd58c0eeff
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-12 19:04:49 +0100

    add: on Sale, Top viewed products

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -488,7 +488,204 @@ function Home() {
             </div>
         </div>
         {/* End Most Popular Area */}
-        
+        {/* Start Shop Home List  */}
+      <section className="shop-home-list section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="row">
+                <div className="col-12">
+                  <div className="shop-section-title">
+                    <h1>On sale</h1>
+                  </div>
+                </div>
+              </div>
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h4 className="title"><a href="#">Coppertone Sport</a></h4>
+                      <p className="price with-discount">$59</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h5 className="title"><a href="#">Coppertone Sport</a></h5>
+                      <p className="price with-discount">$44</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h5 className="title"><a href="#">Coppertone Sport</a></h5>
+                      <p className="price with-discount">$89</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+            </div>
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="row">
+                <div className="col-12">
+                  <div className="shop-section-title">
+                    <h1>Best Seller</h1>
+                  </div>
+                </div>
+              </div>
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h5 className="title"><a href="#">Coppertone Sport</a></h5>
+                      <p className="price with-discount">$65</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h5 className="title"><a href="#">Coppertone Sport</a></h5>
+                      <p className="price with-discount">$33</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h5 className="title"><a href="#">Coppertone Sport</a></h5>
+                      <p className="price with-discount">$77</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+            </div>
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="row">
+                <div className="col-12">
+                  <div className="shop-section-title">
+                    <h1>Top viewed</h1>
+                  </div>
+                </div>
+              </div>
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h5 className="title"><a href="#">Coppertone Sport</a></h5>
+                      <p className="price with-discount">$22</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h5 className="title"><a href="#">Coppertone Sport</a></h5>
+                      <p className="price with-discount">$35</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+              {/* Start Single List  */}
+              <div className="single-list">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <div className="list-image overlay">
+                      <img src="https://via.placeholder.com/115x140" alt="#" />
+                      <a href="#" className="buy"><i className="fa fa-shopping-bag"></i></a>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12 no-padding">
+                    <div className="content">
+                      <h5 className="title"><a href="#">Coppertone Sport</a></h5>
+                      <p className="price with-discount">$99</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* End Single List  */}
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* End Shop Home List  */}
+
     </div>
   )
 }
