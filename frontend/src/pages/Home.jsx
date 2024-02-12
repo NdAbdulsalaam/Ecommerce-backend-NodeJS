88d4719daae0f644d9f8f115f5f5128afd2a816a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-12 18:42:24 +0100

    add: slider for hot items

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -362,7 +362,133 @@ function Home() {
         </div>
       </section>
       {/* End Midium Banner */}
-
+      {/* Start Most Popular  */}
+	    <div class="product-area most-popular section">
+        <div class="container">
+            <div class="row">
+				      <div class="col-12">
+					      <div class="section-title">
+						      <h2>Hot Item</h2>
+					      </div>
+				      </div>
+              </div>
+                <div class="row">
+                  <div class="col-12">
+                    <div class="owl-carousel popular-slider">
+                      {/* Start Single Product */}
+                <div className="single-product">
+                  <div className="product-img">
+                    <a href="product-details.html">
+                      <img className="default-img" src="https://via.placeholder.com/550x750" alt="#" />
+                      <img className="hover-img" src="https://via.placeholder.com/550x750" alt="#" />
+                      <span className="out-of-stock">Hot</span>
+                    </a>
+                    <div className="button-head">
+                      <div className="product-action">
+                        <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i className=" ti-eye"></i><span>Quick Shop</span></a>
+                        <a title="Wishlist" href="#"><i className=" ti-heart "></i><span>Add to Wishlist</span></a>
+                        <a title="Compare" href="#"><i className="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
+                      </div>
+                      <div className="product-action-2">
+                        <a title="Add to cart" href="#">Add to cart</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="product-content">
+                    <h3><a href="product-details.html">Advil (ibuprofen)</a></h3>
+                    <div className="product-price">
+                      <span className="old">$25.00</span>
+                      <span>$20.00</span>
+                    </div>
+                  </div>
+                </div>
+                {/* End Single Product */}
+                {/* Start Single Product */}
+                <div className="single-product">
+                  <div className="product-img">
+                      <a href="product-details.html">
+                          <img className="default-img" src="https://via.placeholder.com/550x750" alt="#" />
+                          <img className="hover-img" src="https://via.placeholder.com/550x750" alt="#" />
+                      </a>
+                    <div className="button-head">
+                      <div className="product-action">
+                        <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i className=" ti-eye"></i><span>Quick Shop</span></a>
+                        <a title="Wishlist" href="#"><i className=" ti-heart "></i><span>Add to Wishlist</span></a>
+                        <a title="Compare" href="#"><i className="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
+                      </div>
+                      <div className="product-action-2">
+                        <a title="Add to cart" href="#">Add to cart</a>
+                      </div>
+                    </div>
+                    </div>
+                    <div className="product-content">
+                        <h3><a href="product-details.html">Zyrtec (cetirizine)</a></h3>
+                        <div className="product-price">
+                            <span>$50.00</span>
+                        </div>
+                    </div>
+                  </div>
+                {/* End Single Product */}
+                {/* Start Single Product */}
+                <div className="single-product">
+                                <div className="product-img">
+                                    <a href="product-details.html">
+                                        <img className="default-img" src="https://via.placeholder.com/550x750" alt="#" />
+                                        <img className="hover-img" src="https://via.placeholder.com/550x750" alt="#" />
+                      <span className="new">New</span>
+                                    </a>
+                    <div className="button-head">
+                      <div className="product-action">
+                        <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i className=" ti-eye"></i><span>Quick Shop</span></a>
+                        <a title="Wishlist" href="#"><i className=" ti-heart "></i><span>Add to Wishlist</span></a>
+                        <a title="Compare" href="#"><i className="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
+                      </div>
+                      <div className="product-action-2">
+                        <a title="Add to cart" href="#">Add to cart</a>
+                      </div>
+                    </div>
+                    </div>
+                    <div className="product-content">
+                        <h3><a href="product-details.html">Nature's Field Vit-E</a></h3>
+                        <div className="product-price">
+                            <span>$50.00</span>
+                        </div>
+                    </div>
+                </div>
+                {/* End Single Product */}
+                {/* Start Single Product */}
+                <div className="single-product">
+                  <div className="product-img">
+                      <a href="product-details.html">
+                          <img className="default-img" src="https://via.placeholder.com/550x750" alt="#" />
+                          <img className="hover-img" src="https://via.placeholder.com/550x750" alt="#" />
+                      </a>
+                    <div className="button-head">
+                      <div className="product-action">
+                        <a data-toggle="modal" data-target="#exampleModal" title="Quick View" href="#"><i className=" ti-eye"></i><span>Quick Shop</span></a>
+                        <a title="Wishlist" href="#"><i className=" ti-heart "></i><span>Add to Wishlist</span></a>
+                        <a title="Compare" href="#"><i className="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
+                      </div>
+                      <div className="product-action-2">
+                        <a title="Add to cart" href="#">Add to cart</a>
+                      </div>
+                    </div>
+                    </div>
+                    <div className="product-content">
+                        <h3><a href="product-details.html">Accu-Chek (by Roche Diabetes Care)</a></h3>
+                        <div className="product-price">
+                            <span>$150.00</span>
+                        </div>
+                    </div>
+                </div>
+                {/* End Single Product */}
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        {/* End Most Popular Area */}
+        
     </div>
   )
 }
