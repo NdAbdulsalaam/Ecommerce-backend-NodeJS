42823b5a13155dd0cf03d198cc56530b7aa238bf
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-12 16:35:48 +0100

    Update changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,33 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      
+      {/* Slider Area */}
+      <section className="hero-slider">
+        {/* Single Slider */}
+        <div className="single-slider">
+          <div className="container">
+            <div className="row no-gutters">
+              <div className="col-lg-9 offset-lg-3 col-12">
+                <div className="text-inner">
+                  <div className="row">
+                    <div className="col-lg-7 col-12">
+                      <div className="hero-text">
+                        <h1><span>UP TO 50% OFF </span>Shirt For Man</h1>
+                        <p>Maboriosam in a nesciung eget magnae <br> dapibus disting tloctio in the find it pereri </br> odiy maboriosm.</p>
+                        <div className="button">
+                          <a href="#" className="btn">Shop Now!</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*/ End Single Slider */}
+      </section>
+      {/*/ End Slider Area */}
     </div>
   )
 }
