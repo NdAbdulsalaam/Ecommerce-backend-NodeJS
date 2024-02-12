e72ff3690154907826babf87f82b0d2004339924
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-12 17:12:08 +0100

    Add: small banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -32,7 +32,50 @@ function Home() {
       </section>
       {/* End Slider Area */}
 
-      
+      {/* Start Small Banner  */}
+      <section className="small-banner section">
+        <div className="container-fluid">
+          <div className="row">
+            {/* Single Banner  */}
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="single-banner">
+                <img src="https://via.placeholder.com/600x370" alt="#" /> 
+                <div className="content">
+                  <p>Medications</p>
+                  <h3>Over-the-Counter <br /> Medications</h3>
+                  <a href="#">Discover Now</a>
+                </div>
+              </div>
+            </div>
+            {/* /End Single Banner  */}
+                        {/* Single Banner  */}
+                        <div className="col-lg-4 col-md-6 col-12">
+              <div className="single-banner">
+                <img src="https://via.placeholder.com/600x370" alt="#" />
+                <div className="content">
+                  <p>Health Supplements</p>
+                  <h3>Multivitamins <br /> 2024</h3>
+                  <a href="#">Shop Now</a>
+                </div>
+              </div>
+            </div>
+            {/* /End Single Banner  */}
+            {/* Single Banner  */}
+            <div className="col-lg-4 col-12">
+              <div className="single-banner tab-height">
+                <img src="https://via.placeholder.com/600x370" alt="#" />
+                <div className="content">
+                  <p>Personal Care</p>
+                  <h3>Skin, Hair & Oral Care <br /> Up to <span>40%</span> Off</h3>
+                  <a href="#">Discover Now</a>
+                </div>
+              </div>
+            </div>
+            {/* /End Single Banner  */}
+          </div>
+        </div>
+      </section>
+      {/* End Small Banner */}
 
     </div>
   )
