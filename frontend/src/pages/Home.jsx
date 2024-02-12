5f0fad6540a30e1cda513013319cf5d33f0d9a8a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-02-12 19:33:25 +0100

    add: shop service area

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -764,7 +764,51 @@ function Home() {
         </div>
       </section>
       {/* End Shop Blog  */}
-
+      {/* Start Shop Services Area */}
+      <section className="shop-services section home">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3 col-md-6 col-12">
+              {/* Start Single Service */}
+              <div className="single-service">
+                <i className="ti-rocket"></i>
+                <h4>Free shiping</h4>
+                <p>Orders over $100</p>
+              </div>
+              {/* End Single Service */}
+            </div>
+            <div className="col-lg-3 col-md-6 col-12">
+              {/* Start Single Service */}
+              <div className="single-service">
+                <i className="ti-reload"></i>
+                <h4>Free Return</h4>
+                <p>Within 30 days returns</p>
+              </div>
+              {/* End Single Service */}
+            </div>
+            <div className="col-lg-3 col-md-6 col-12">
+              {/* Start Single Service */}
+              <div className="single-service">
+                <i className="ti-lock"></i>
+                <h4>Sucure Payment</h4>
+                <p>100% secure payment</p>
+              </div>
+              {/* End Single Service */}
+            </div>
+            <div className="col-lg-3 col-md-6 col-12">
+              {/* Start Single Service */}
+              <div className="single-service">
+                <i className="ti-tag"></i>
+                <h4>Best Peice</h4>
+                <p>Guaranteed price</p>
+              </div>
+              {/* End Single Service */}
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* End Shop Services Area */}
+      
     </div>
   )
 }
